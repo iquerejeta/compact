eeb3a4116c6bbac9fd0fa31178072a66fd939a3e
Leszek Lorek <leszek.lorek@shielded.io>
2025-11-05 14:06:40 +0000

    test: PM-19907 (#51)
    - added tests

---
+++ tests-e2e/src/tests/compiler/compiler.bugs.pm19907.e2e.test.ts
@@ -0,0 +1,177 @@
+// This file is part of Compact.
+// Copyright (C) 2025 Midnight Foundation
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Result } from 'execa';
+import { describe, test } from 'vitest';
+import { Arguments, compile, compilerDefaultOutput, createTempFolder, expectCompilerResult, expectFiles, buildPathTo } from '@';
+
+describe('[Bug] [PM-19907] Consts in single statements', () => {
+    const CONTRACTS_ROOT = buildPathTo('/bugs/pm-19907/');
+
+    test('example contract should be compiled successfully', async () => {
+        const filePath = CONTRACTS_ROOT + 'examples.compact';
+
+        const outputDir = createTempFolder();
+        const result: Result = await compile([Arguments.SKIP_ZK, filePath, outputDir]);
+
+        expectCompilerResult(result).toBeSuccess('', compilerDefaultOutput());
+        expectFiles(outputDir).thatGeneratedJSCodeIsValid();
+    });
+
+    describe('should fail with proper error in certain cases', () => {
+        test('example 1 - single if with const', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_one.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_one.compact line 17 char 18: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 2 - if else with const', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_two.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_two.compact line 17 char 21: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 3 - use witness with if', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_three.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_three.compact line 19 char 26: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 4 - if with vector', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_four.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_four.compact line 17 char 21: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 5 - nested if', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_five.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_five.compact line 19 char 22: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 6 - struct with if', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_six.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_six.compact line 22 char 21: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 7 - single if in constructor', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_seven.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_seven.compact line 17 char 18: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 8 - short hand if', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_eight.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_eight.compact line 18 char 27: parse error: found "const" looking for an expression',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 9 - map with const', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_nine.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_nine.compact line 18 char 24: parse error: found "const" looking for a block or an expression',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 10 - for with const', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_ten.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_ten.compact line 18 char 31: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 11 - if and else with const', async () => {
+            const filePath = CONTRACTS_ROOT + 'negative/example_eleven.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_eleven.compact line 20 char 16: const binding found in a single-statement context',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+    });
+});
