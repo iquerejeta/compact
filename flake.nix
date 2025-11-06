8aaca36fbae2d8424b52ff0facf6d8964af223a2
leszek.lorek <leszek.lorek@shielded.io>
2025-11-06 16:08:43 +0000

    test: PM-20231
    - added and fixed tests

--- flake.nix
+++ flake.nix
@@ -209,7 +209,7 @@
 
           packages.compactc = pkgs.stdenv.mkDerivation {
             name = "compactc";
-            version = "0.26.118"; # NB: also update compiler-version in compiler/compiler-version.ss
+            version = "0.26.119"; # NB: also update compiler-version in compiler/compiler-version.ss
             src = inclusive.lib.inclusive ./. [
               ./test-center
               ./compiler
