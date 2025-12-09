a514d7d924bdc11aa720f4e64f7467d5fa7d35ed
Thomas Kerber <thomas.kerber@iohk.io>
2025-12-09 13:01:32 +0000

    Bump to ledger-6.2.0-rc.2.

--- flake.nix
+++ flake.nix
@@ -31,18 +31,18 @@
       # NOTE: if this is an internal release (uses -alpha, -beta, or -rc) do NOT update the package.json in runtime
       # since npm can only access public releases. For the compact-runtime release nix will pull in the correct
       # version from this url.
-      url = "github:midnightntwrk/midnight-ledger/tkerber/patch-midnight-zk";
+      url = "github:midnightntwrk/midnight-ledger/ledger-6.2.0-rc.2";
       inputs.zkir.follows = "zkir";
     };
     onchain-runtime-v1 = {
       # dependency for compact-runtime release
       # all notes for the zkir input applies to onchain-runtime input too.
-      url = "github:midnightntwrk/midnight-ledger/ledger-6.2.0-rc.1";
+      url = "github:midnightntwrk/midnight-ledger/ledger-6.2.0-rc.2";
       inputs.zkir.follows = "zkir";
     };
     zkir-wasm = {
       # dependency for test-center
-      url = "github:midnightntwrk/midnight-ledger/ledger-6.2.0-rc.1";
+      url = "github:midnightntwrk/midnight-ledger/ledger-6.2.0-rc.2";
       inputs.zkir.follows = "zkir";
     };
     n2c.url = "github:nlewo/nix2container";
