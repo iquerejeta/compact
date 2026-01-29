b8d6d0c1e56b91ff902a15efc046ce4bf6ec6258
leszek.lorek <leszek.lorek@shielded.io>
2026-01-29 15:44:32 +0000

    test: PM-21275
    - added tests
    
    Signed-off-by: leszek.lorek <leszek.lorek@shielded.io>

---
+++ tests-e2e/src/tests/compiler/compiler.return.e2e.test.ts
@@ -0,0 +1,74 @@
+// This file is part of Compact.
+// Copyright (C) 2025 Midnight Foundation
+// SPDX-License-Identifier: Apache-2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//  	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { Result } from 'execa';
+import { describe, test } from 'vitest';
+import { Arguments, compile, compilerDefaultOutput, createTempFolder, expectCompilerResult, expectFiles, buildPathTo } from '@';
+
+describe('[Return] [PM-21275] Better error message when contract fails to return the required type of value', () => {
+    const CONTRACTS_ROOT = buildPathTo('/return/');
+    const CONTRACTS_NEGATIVE_ROOT = buildPathTo('/return/negative/');
+
+    test('example contract should be compiled successfully', async () => {
+        const filePath = CONTRACTS_ROOT + 'examples.compact';
+
+        const outputDir = createTempFolder();
+        const result: Result = await compile([Arguments.SKIP_ZK, filePath, outputDir]);
+
+        expectCompilerResult(result).toBeSuccess('', compilerDefaultOutput());
+        expectFiles(outputDir).thatGeneratedJSCodeIsValid();
+    });
+
+    describe('should fail with proper error in certain cases', () => {
+        test('example 1 - usage of short if', async () => {
+            const filePath = CONTRACTS_NEGATIVE_ROOT + 'example_one.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_one.compact line 17 char 10: parse error: found keyword "return" looking for an expression',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 2 - one path not covered', async () => {
+            const filePath = CONTRACTS_NEGATIVE_ROOT + 'example_two.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_two.compact line 16 char 1: circuit test is declared to return a value of type Field, but its body can return without supplying a value',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+
+        test('example 3 - all paths covered (one not needed)', async () => {
+            const filePath = CONTRACTS_NEGATIVE_ROOT + 'example_three.compact';
+
+            const outputDir = createTempFolder();
+            const result: Result = await compile([Arguments.VSCODE, filePath, outputDir]);
+
+            expectCompilerResult(result).toBeFailure(
+                'Exception: example_three.compact line 18 char 3: unreachable statement',
+                compilerDefaultOutput(),
+            );
+            expectFiles(outputDir).thatNoFilesAreGenerated();
+        });
+    });
+});
